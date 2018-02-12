b455f59281bf802cd67e1a4da76578643c99f623
Burdened <36141989+procrastinates@users.noreply.github.com>
2018-02-12 10:05:01 -0600

    index.php version 0.0.1
    Setting up dependencies.

---
+++ main/index.php
@@ -0,0 +1,36 @@
+<?php 
+/*
+Written by https://github.com/procrastinates
+If you remove this line and the one above it will be considered a nulled copy, and you may face penalities for it.
+This file is protected by the GNU Affero General Public License v.3
+A copy of the license will be included in each and every build
+*/
+
+require 'conn.php';
+
+/* I will work more on the backend functions, but for now I will just document most of my code. */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<!--
+Written by https://github.com/procrastinates
+If you remove this line and the one above it will be considered a nulled copy, and you may face penalities for it.
+This file is protected by the GNU Affero General Public License v.3
+A copy of the license will be included in each and every build
+-->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+<!--- <nav> todo: work on the navbar </nav> -->
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!-- Coded by https://github.com/procrastinates -->
+</body>
+</html>
