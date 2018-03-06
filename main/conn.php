7ad491178a3a5585171e2d4e05c8afb3a33176b1
Burdened <36141989+procrastinates@users.noreply.github.com>
2018-03-06 10:09:46 -0600

    Create conn.php

---
+++ main/conn.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Written by https://github.com/procrastinates
+If you remove this line and the one above it will be considered a nulled copy, and you may face penalities for it.
+This file is protected by the GNU Affero General Public License v.3
+A copy of the license will be included in each and every build
+*/
+require 'config.php'; 
+
+/*
+
+DO NOT EDIT ME DIRECTLY!
+Please use config.php to edit values, I promise this is for the confusing stuff and magical stuff.
+
+*/
+
+$SEHABTy5t4dCBkkyHFNzM8mcM7t3qzQw = $host;
+$BFSPELvxnnwSa9z757jbDHANeUsmmZTV = $udb;
+$gLzwJKn3S9TyaEm42CVS3HrwwqGPtF8B = $pdb;
+$xwhn344bkCrE3BgN55A3mShnhV6YWArY = $db;
+
+$conn = new mysqli($SEHABTy5t4dCBkkyHFNzM8mcM7t3qzQw, $BFSPELvxnnwSa9z757jbDHANeUsmmZTV, $gLzwJKn3S9TyaEm42CVS3HrwwqGPtF8B, $xwhn344bkCrE3BgN55A3mShnhV6YWArY)
+
+/* Will be working on error exceptions soon. */
